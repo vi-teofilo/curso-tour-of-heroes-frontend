5454930b820a7a53d0015fdbdbebf59ca74400d6
vi-teofilo <vitoria.teofilovt@gmail.com>
2023-01-27 14:34:22 -0300

    fix: criacao da classe message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { HeroService } from '../service/hero.service';
 
 import { Hero } from './heroes.model';
@@ -19,8 +20,65 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes =>
-      this.heroes = heroes);
+    // usando o of na service ele ja faz o next e o complete por isso nos possibilita fazer a utilizacao de uma forma sucinta
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+
+    // Para recuperar os valores e Tratar o erro se necessario Observable: mais detalhes (https://rxjs.dev/guide/observable).
+    // this.heroService.getHeroes().subscribe({
+    //   next(x) {
+    //     console.log('got value ' + JSON.stringify(x, null, 2));
+    //   },
+    //   error(err) {
+    //     console.error('something wrong occurred: ' + err);
+    //   },
+    //   complete() {
+    //     console.log('done');
+    //   },
+    // });
+
+    // Forma menos verbosa e mais simples que o JS proporciona.
+    // this.heroService.getHeroes().subscribe(
+    //   (value) => {
+    //     console.log(value);
+    //   },
+    //   (err) => {
+    //     console.log(err);
+    //   },
+    //   () => {
+    //     console.log('foi');
+    //   }
+    // );
+
+    // Forma menos verbosa e mais simples que o JS proporciona.
+    // this.heroService.getHeroes().subscribe(
+    //   (value) => console.log(value),
+    //   (err) => console.log(err),
+    //   () => console.log('foi')
+    // );
+
+    // const foo = new Observable((subscriber) => {
+    //   subscriber.next(1);
+    //   subscriber.next(2);
+    //   subscriber.next(3);
+    //   setTimeout(() => {
+    //     subscriber.next(4); // happens asynchronously
+    //     subscriber.complete();
+    //   }, 1000);
+    // });
+
+    // console.log('before');
+    // foo.subscribe({
+    //   next(x) {
+    //     console.log('got value ' + x);
+    //   },
+    //   error(err) {
+    //     console.error('something wrong occurred: ' + err);
+    //   },
+    //   complete() {
+    //     console.log('done');
+    //   },
+    // });
+    // console.log('after');
   }
 
   onSelect(hero: Hero): void {
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { Hero } from '../heroes/heroes.model';
 import { HEROES } from '../mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  constructor(private messageService: MessageService){
+
+  }
+
   getHeroes(): Observable<Hero[]> {
+
     const heroes = of(HEROES)
+    this.messageService.add('HeroService: fetched heroes')
+    //return throwError(new Error('Errouu'))
     return heroes;
   }
 }
