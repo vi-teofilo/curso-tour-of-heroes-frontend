d4f335ea77c3beccdfc99ce433e5a5590df0ebc5
vi-teofilo <vitoria.teofilovt@gmail.com>
2023-02-07 14:18:22 -0300

    fix: estruturando o core da aplicacao

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { MenuItem } from '../../models/menu-item.model';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,4 +8,5 @@ import { Component, Input } from '@angular/core';
 })
 export class ToolbarComponent {
  @Input() title = '';
+ @Input() menuItems: MenuItem[] = []
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
 
 const COMPONENTS = [MessagesComponent, ToolbarComponent]
 const MODULES = [FlexLayoutModule,MaterialModule ]
@@ -10,8 +12,14 @@ const MODULES = [FlexLayoutModule,MaterialModule ]
 @NgModule({
   declarations: [COMPONENTS],
   imports: [
-    MODULES
+    CommonModule,MODULES,AppRoutingModule
   ],
   exports:[COMPONENTS, MODULES],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parantModule?: CoreModule){
+    if (parantModule) {
+      throw new Error('CoreModule has already been loaded.import this module in the AppModule')
+    }
+  }
+}
